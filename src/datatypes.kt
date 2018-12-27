04251d14fbaa21e49b7a24b43bcf4b15c642e8a3
Tech Spandan <thetechspandan@gmail.com>
2018-12-27 18:16:50 +0530

    basic datatypes demo

---
+++ src/datatypes.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+    //Byte datatype
+    var byteNumber: Byte = 12
+    println(byteNumber)
+
+    //Short datatype
+    var shortNumber: Short = 3434
+    println(shortNumber)
+
+    //Int datatype
+    var intNumber: Int = 3434343
+    println(intNumber)
+
+    //Long datatype
+    var longNumber: Long = 3443
+    println(longNumber)
+
+    //Float datatype
+    var floatNumber: Float = 23.4f
+    println(floatNumber)
+
+    //Double datatype
+    var doubleNumber: Double = 4.5
+    println(doubleNumber)
+
+    //Char datatype
+    var firstChar: Char = 'a'
+    println(firstChar)
+
+    //Boolean datatype
+    var booleanVariable: Boolean = true
+    println(booleanVariable)
+}
